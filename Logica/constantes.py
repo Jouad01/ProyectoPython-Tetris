348ada8e42b89591647410953b1df45be9ec22d9
jouad01 <jouardiouardi@cifpfbmoll.eu>
2022-12-25 01:23:56 +0100

    feat: creados datos de piezas

---
+++ Logica/constantes.py
@@ -0,0 +1,18 @@
+# Fichero que contendrá el tiempo de caida de piezas, el ancho y los colores
+
+BOARD_WIDTH = 10
+BOARD_HEIGHT = 20
+BLOCK_SIZE = 30
+
+
+SPEED_MS = 500
+
+COLOR = {
+    "rojo": (255, 0, 0),
+    "verde": (0, 255, 0),
+    "azul": (0, 0, 255),
+    "amarillo": (255, 255, 0),
+    "morado": (255, 0, 255),
+    "naranja": (255, 128, 0),
+    # Añadir más
+}
