1671f26d1cc9efe49420520fce44afb103b4e8a3
jouad01 <jouardiouardi@cifpfbmoll.eu>
2023-01-06 22:08:25 +0100

    refactor: cambiada estructura de directorios

--- pruebas.py
+++
@@ -1,7 +0,0 @@
-def main():
-    outfile = open("notas.txt","w")
-    fname = input("Introduzca su nombre de usuario: ")
-    outfile.write(fname)
-    outfile.write("\t")
-    outfile.close()
-main()
