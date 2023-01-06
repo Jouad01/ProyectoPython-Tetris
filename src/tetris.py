d36887425031f3818132a351b30e95b4eee7aaa5
jouad01 <jouardiouardi@cifpfbmoll.eu>
2023-01-06 22:17:39 +0100

    feat: fichero main que ejecutara el juego

---
+++ src/tetris.py
@@ -0,0 +1,24 @@
+import pygame
+import random
+
+from constantes import *
+from game import *
+
+
+pygame.font.init()
+
+def main(win): 
+    last_score = max_score()
+    locked_positions = {}
+    grid = create_grid(locked_positions)
+
+    change_piece = False
+    run = True
+    current_piece = get_shape()
+    next_piece = get_shape()
+    clock = pygame.time.Clock()
+    fall_time = 0
+    fall_speed = 0.27
+    level_time = 0
+    score = 0
+
