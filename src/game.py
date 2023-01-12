3ea5d21a01e864102b8e4c54dfa565bda2b68de0
jouad01 <jouardiouardi@cifpfbmoll.eu>
2023-01-12 16:44:34 +0100

    fix: arreglado problema con scores

--- src/game.py
+++ src/game.py
@@ -12,12 +12,11 @@ class Piece(object):
         self.color = shape_colors[shapes.index(shape)]
         self.rotation = 0
 
-# crea una array de 20 filas y 10 columnas que representa el tablero del Tetris
+# crea una array llamada grid de 20 filas y 10 columnas que representa el tablero del Tetris
 # si la coordenada de la celda está en el diccionario locked_pos, se asigna el color de la celda desde el diccionario 
 # de lo contrario, se asigna el color negro a la celda.
 def create_grid(locked_pos={}): 
     grid = [[(0,0,0) for _ in range(10)] for _ in range(20)]
-
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if (j, i) in locked_pos:
@@ -125,15 +124,15 @@ def draw_next_shape(shape, surface):
 
 
 # Para guardar la puntuacion en un fichero 
-# Pendiente mejorar
-def update_score(nscore):
+# Pendiente registrar usuario y guardar puntuacion por usuario
+def update_score(actual_score):
     score = max_score()
 
-    with open('utilities/scores.txt'):
-        if int(score) > nscore:
+    with open('utilities/scores.txt', 'w') as f:
+        if int(score) > actual_score:
             f.write(str(score))
         else:
-            f.write(str(nscore))
+            f.write(str(actual_score))
 
 def max_score():
     with open('utilities/scores.txt', 'r') as f:
@@ -162,7 +161,7 @@ def draw_window(surface, grid, score=0, last_score = 0):
 
     surface.blit(label, (sx + 20, sy + 160))
     # puntuacion maxima
-    label = font.render('Record: ' + last_score, 1, (255,255,255))
+    label = font.render('Tu record: ' + last_score, 1, (255,255,255))
 
     sx = LEFT_X - 200
     sy = LEFT_Y + 200
