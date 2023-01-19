6119e554ead37320c35fd3203b1689ac77024531
Noelia <ncrespipomar@cifpfbmoll.eu>
2023-01-19 16:18:14 +0100

    test: board ancho

--- src/tests/test_board.py
+++ src/tests/test_board.py
@@ -4,3 +4,7 @@ from board import *
 class TestBoard(unittest.TestCase):
     def test_width(self):
         self.assertEqual(WIDTH, 900)
+    
+    def test_height(self):
+        self.assertEqual(HEIGHT, 700)
+        
