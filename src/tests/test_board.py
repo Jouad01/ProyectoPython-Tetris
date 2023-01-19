a3ad5f98b294998c36255c8453bb088bc3bbb9a4
Noelia <ncrespipomar@cifpfbmoll.eu>
2023-01-19 16:26:48 +0100

    test: shape

--- src/tests/test_board.py
+++ src/tests/test_board.py
@@ -22,3 +22,14 @@ class TestBoard(unittest.TestCase):
 
     def test_left_y(self):
         self.assertEqual(LEFT_Y, HEIGHT - TETRIS_HEIGHT)
+    
+    def test_shapes(self):
+        self.assertIsInstance(shapes, list)
+        self.assertGreater(len(shapes), 0)
+
+    def test_shape_colors(self):
+        self.assertIsInstance(shape_colors, list)
+        self.assertGreater(len(shape_colors), 0)
+
+if __name__ == '__main__':
+    unittest.main()
